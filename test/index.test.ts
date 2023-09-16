c43e42221cfa1e21d35e9020452485f63914ff6b
Sebastian Siedler <55579075+SebastianSiedler@users.noreply.github.com>
2023-09-16 14:59:44 +0200

    add getClubInformation Method

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,7 @@
+import exp from "constants";
 import { api } from "../src/client";
 import { describe, expect, it } from "vitest";
+import z from "zod";
 
 const teamPermanentId = "016PD5QT70000000VV0AG811VTE5EA5R";
 
@@ -14,5 +16,35 @@ describe("get matches", () => {
     expect(matches).toBeDefined();
 
     expect(matches.length).toBeGreaterThan(0);
+  });
+});
+
+describe("club information", () => {
+  it("get club information", async () => {
+    const { data } = await api.getClubInformation({
+      queries: { teamPermanentId },
+    });
+
+    expect(data?.club).toBeDefined();
+    expect(data?.club?.name).toBe("TSC Zeuzleben");
+    expect(data?.club?.logoPublic).toBeTypeOf("boolean");
+  });
+
+  it("get club information with wrong teamPermanentId", async () => {
+    const getClubinfo = async () =>
+      await api.getClubInformation({
+        queries: { teamPermanentId: "aaaabaaaacaaaabaaaacaaaabaaaac12" },
+      });
+
+    expect(getClubinfo).rejects.toThrowError("Expected object, received null");
+  });
+
+  it("get club information with wrong length teamPermanentId", async () => {
+    const getClubinfo = async () =>
+      await api.getClubInformation({
+        queries: { teamPermanentId: "wrongLength" },
+      });
+
+    expect(getClubinfo).rejects.toThrowError("Invalid Query parameter");
   });
 });
